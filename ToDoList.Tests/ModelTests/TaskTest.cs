1c1a127123cbda8342e706e0095041967a5db38d
Kimberly Bordon <kbordon@gmail.com>
2017-10-23 22:17:08 -0700

    Enable category to access database.

--- ToDoList.Tests/ModelTests/TaskTest.cs
+++ ToDoList.Tests/ModelTests/TaskTest.cs
@@ -1,8 +1,7 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Collections.Generic;
-using ToDoList.Models;
-using ToDoList;
 using System;
+using ToDoList.Models;
 
 namespace ToDoList.Tests
 {
@@ -10,59 +9,49 @@ namespace ToDoList.Tests
     [TestClass]
     public class TaskTests : IDisposable
     {
-        public void Dispose()
-        {
-            Task.DeleteAll();
-        }
         public TaskTests()
         {
             DBConfiguration.ConnectionString = "server=localhost;user id=root;password=root;port=3306;database=todo_test;";
         }
-
-        [TestMethod]
-        public void GetAll_DatabaseEmptyAtFirst_0()
+        public void Dispose()
         {
-          //Arrange, Act
-          int result = Task.GetAll().Count;
-
-          //Assert
-          Assert.AreEqual(0, result);
+          Task.DeleteAll();
+          Category.DeleteAll();
         }
 
         [TestMethod]
-        public void Save_SavesToDatabase_TaskList()
+        public void Equals_OverrideTrueForSameDescription_Task()
+        {
+          //Arrange, Act
+          Task firstTask = new Task("Mow the lawn", 1);
+          Task secondTask = new Task("Mow the lawn", 1);
+
+          //Assert
+          Assert.AreEqual(firstTask, secondTask);
+        }
+
+        [TestMethod]
+        public void Save_SavesTaskToDatabase_TaskList()
         {
           //Arrange
-          Task testTask = new Task("Mow the lawn");
+          Task testTask = new Task("Mow the lawn", 1);
+          testTask.Save();
 
           //Act
-          testTask.Save();
           List<Task> result = Task.GetAll();
           List<Task> testList = new List<Task>{testTask};
 
           //Assert
           CollectionAssert.AreEqual(testList, result);
         }
-
-        [TestMethod]
-        public void Equals_ReturnsTrueIfDescriptionsAreTheSame_Task()
-        {
-          // Arrange, Act
-          Task firstTask = new Task("Mow the lawn");
-          Task secondTask = new Task("Mow the lawn");
-
-          // Assert
-          Assert.AreEqual(firstTask, secondTask);
-        }
-
-        [TestMethod]
-        public void Save_AssignsIdToObject_Id()
+       [TestMethod]
+        public void Save_DatabaseAssignsIdToObject_Id()
         {
           //Arrange
-          Task testTask = new Task("Mow the lawn");
+          Task testTask = new Task("Mow the lawn", 1);
+          testTask.Save();
 
           //Act
-          testTask.Save();
           Task savedTask = Task.GetAll()[0];
 
           int result = savedTask.GetId();
@@ -72,5 +61,36 @@ namespace ToDoList.Tests
           Assert.AreEqual(testId, result);
         }
 
+        [TestMethod]
+        public void Find_FindsTaskInDatabase_Task()
+        {
+          //Arrange
+          Task testTask = new Task("Mow the lawn", 1);
+          testTask.Save();
+
+          //Act
+          Task foundTask = Task.Find(testTask.GetId());
+
+          //Assert
+          Assert.AreEqual(testTask, foundTask);
+        }
+
+        [TestMethod]
+        public void GetTasks_RetrievesAllTasksWithCategory_TaskList()
+        {
+          Category testCategory = new Category("Household chores");
+          testCategory.Save();
+
+          Task firstTask = new Task("Mow the lawn", testCategory.GetId());
+          firstTask.Save();
+          Task secondTask = new Task("Do the dishes", testCategory.GetId());
+          secondTask.Save();
+
+
+          List<Task> testTaskList = new List<Task> {firstTask, secondTask};
+          List<Task> resultTaskList = testCategory.GetTasks();
+
+          CollectionAssert.AreEqual(testTaskList, resultTaskList);
+        }
     }
 }
--- ToDoList/Models/Task.cs
+++ ToDoList/Models/Task.cs
@@ -8,15 +8,13 @@ namespace ToDoList.Models
     {
         private string _description;
         private int _id;
-        // private static List<Task> _instances = new List<Task> {};
-        // changed w/database
+        private int _categoryId;
 
-        public Task (string Description, int Id = 0)
+        public Task(string description, int categoryId, int id = 0)
         {
-            _description = Description;
-            _id = Id;
-            // _instances.Add(this);
-            // _id = _instances.Count;  Changed w/database
+            _description = description;
+            _categoryId = categoryId;
+            _id = id;
         }
 
         public override bool Equals(System.Object otherTask)
@@ -27,123 +25,125 @@ namespace ToDoList.Models
           }
           else
           {
-            Task newTask = (Task) otherTask;
-            bool idEquality = (this.GetId() == newTask.GetId());
-            bool descriptionEquality = (this.GetDescription() == newTask.GetDescription());
-            return (idEquality && descriptionEquality);
-          }
+             Task newTask = (Task) otherTask;
+             bool idEquality = this.GetId() == newTask.GetId();
+             bool descriptionEquality = this.GetDescription() == newTask.GetDescription();
+             bool categoryEquality = this.GetCategoryId() == newTask.GetCategoryId();
+             return (idEquality && descriptionEquality && categoryEquality);
+           }
         }
+        public override int GetHashCode()
+        {
+             return this.GetDescription().GetHashCode();
+        }
+
         public string GetDescription()
         {
             return _description;
         }
-
-        public void SetDescription(string newDescription)
-        {
-            _description = newDescription;
-        }
-
         public int GetId()
         {
             return _id;
         }
-        public static List<Task> GetAll()
+        public int GetCategoryId()
         {
-            // return _instances; Changed w/database
-            List<Task> allTasks = new List<Task> {};
-            MySqlConnection conn = DB.Connection();
-            conn.Open();
-            MySqlCommand cmd = conn.CreateCommand() as MySqlCommand;
-            cmd.CommandText = @"SELECT * FROM tasks;";
-            MySqlDataReader rdr = cmd.ExecuteReader() as MySqlDataReader;
-            while(rdr.Read())
-            {
-             int taskId = rdr.GetInt32(0);
-             string taskDescription = rdr.GetString(1);
-             Task newTask = new Task(taskDescription, taskId);
-             allTasks.Add(newTask);
-            }
-            conn.Close();
-            if (conn != null)
-            {
-               conn.Dispose();
-            }
-            return allTasks;
+            return _categoryId;
         }
-
         public void Save()
         {
             MySqlConnection conn = DB.Connection();
             conn.Open();
 
             var cmd = conn.CreateCommand() as MySqlCommand;
-            cmd.CommandText = @"INSERT INTO `tasks` (`description`) VALUES (@TaskDescription);";
+            cmd.CommandText = @"INSERT INTO tasks (description, category_id) VALUES (@description, @category_id);";
 
             MySqlParameter description = new MySqlParameter();
-            description.ParameterName = "@TaskDescription";
+            description.ParameterName = "@description";
             description.Value = this._description;
             cmd.Parameters.Add(description);
 
+            MySqlParameter categoryId = new MySqlParameter();
+            categoryId.ParameterName = "@category_id";
+            categoryId.Value = this._categoryId;
+            cmd.Parameters.Add(categoryId);
+
+
             cmd.ExecuteNonQuery();
             _id = (int) cmd.LastInsertedId;
-
-             conn.Close();
-             if (conn != null)
-             {
-                 conn.Dispose();
-             }
+            conn.Close();
+            if (conn != null)
+            {
+                conn.Dispose();
+            }
         }
-        public static void DeleteAll()
+
+        public static List<Task> GetAll()
         {
+            List<Task> allTasks = new List<Task> {};
             MySqlConnection conn = DB.Connection();
             conn.Open();
-
             var cmd = conn.CreateCommand() as MySqlCommand;
-            cmd.CommandText = @"DELETE FROM tasks;";
-
-            cmd.ExecuteNonQuery();
-
-             conn.Close();
-             if (conn != null)
-             {
-                 conn.Dispose();
-             }
+            cmd.CommandText = @"SELECT * FROM tasks;";
+            var rdr = cmd.ExecuteReader() as MySqlDataReader;
+            while(rdr.Read())
+            {
+              int taskId = rdr.GetInt32(0);
+              string taskDescription = rdr.GetString(1);
+              int taskCategoryId = rdr.GetInt32(2);
+              Task newTask = new Task(taskDescription, taskCategoryId, taskId);
+              allTasks.Add(newTask);
+            }
+            conn.Close();
+            if (conn != null)
+            {
+                conn.Dispose();
+            }
+            return allTasks;
         }
-
         public static Task Find(int id)
         {
             MySqlConnection conn = DB.Connection();
             conn.Open();
-
             var cmd = conn.CreateCommand() as MySqlCommand;
-            cmd.CommandText = @"SELECT * FROM `tasks` WHERE id = @thisId;";
+            cmd.CommandText = @"SELECT * FROM tasks WHERE id = (@searchId);";
 
-            MySqlParameter thisId = new MySqlParameter();
-            thisId.ParameterName = "@thisId";
-            thisId.Value = id;
-            cmd.Parameters.Add(thisId);
+            MySqlParameter searchId = new MySqlParameter();
+            searchId.ParameterName = "@searchId";
+            searchId.Value = id;
+            cmd.Parameters.Add(searchId);
 
             var rdr = cmd.ExecuteReader() as MySqlDataReader;
-
             int taskId = 0;
-            string taskDescription = "";
+            string taskName = "";
+            int taskCategoryId = 0;
 
-            while (rdr.Read())
+            while(rdr.Read())
             {
-                taskId = rdr.GetInt32(0);
-                taskDescription = rdr.GetString(1);
+              taskId = rdr.GetInt32(0);
+              taskName = rdr.GetString(1);
+              taskCategoryId = rdr.GetInt32(2);
             }
+            Task newTask = new Task(taskName, taskCategoryId, taskId);
+            conn.Close();
+            if (conn != null)
+            {
+                conn.Dispose();
+            }
+            return newTask;
+        }
 
-            Task foundTask= new Task(taskDescription, taskId);  // This line is new!
-
-             conn.Close();
-             if (conn != null)
-             {
-                 conn.Dispose();
-             }
-
-            return foundTask;  // This line is new!
-
+        public static void DeleteAll()
+        {
+            MySqlConnection conn = DB.Connection();
+            conn.Open();
+            var cmd = conn.CreateCommand() as MySqlCommand;
+            cmd.CommandText = @"DELETE FROM tasks;";
+            cmd.ExecuteNonQuery();
+            conn.Close();
+            if (conn != null)
+            {
+                conn.Dispose();
+            }
         }
     }
 }
